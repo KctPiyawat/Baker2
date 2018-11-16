59122debe5e031af627d7f9cdd5375bfdc59ba12
PI <abc@gmail.com>
2018-11-16 15:25:35 +0700

    node Server Succes

---
+++ node/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) =>{res.end('Welcome to Root Path')})
+app.get('/api',(req,res) => {res.end('This is API')})
+
+app.listen(3000,() => {console.log('Sever Node Working123')})
